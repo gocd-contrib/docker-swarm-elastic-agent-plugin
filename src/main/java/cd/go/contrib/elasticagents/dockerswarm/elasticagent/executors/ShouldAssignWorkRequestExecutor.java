a944c4ca74c9082b02c793b5925e1377eb047320
Bhupendrakumar Piprava <bdpiparva@gmail.com>
2018-02-22 12:33:24 +0530

    Removed fallback strategy from should assign work. Plugin checks job identifier on agent to assign work to agent.

--- src/main/java/cd/go/contrib/elasticagents/dockerswarm/elasticagent/executors/ShouldAssignWorkRequestExecutor.java
+++ src/main/java/cd/go/contrib/elasticagents/dockerswarm/elasticagent/executors/ShouldAssignWorkRequestExecutor.java
@@ -23,12 +23,8 @@ import cd.go.contrib.elasticagents.dockerswarm.elasticagent.requests.ShouldAssig
 import com.thoughtworks.go.plugin.api.response.DefaultGoPluginApiResponse;
 import com.thoughtworks.go.plugin.api.response.GoPluginApiResponse;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import static cd.go.contrib.elasticagents.dockerswarm.elasticagent.DockerPlugin.LOG;
 import static java.text.MessageFormat.format;
-import static org.apache.commons.lang.StringUtils.stripToEmpty;
 
 
 public class ShouldAssignWorkRequestExecutor implements RequestExecutor {
@@ -54,22 +50,7 @@ public class ShouldAssignWorkRequestExecutor implements RequestExecutor {
             return DefaultGoPluginApiResponse.success("true");
         }
 
-        if (matchUsingProfileAndEnvironment(instance)) {
-                LOG.info(format("[should-assign-work] Job with profile {0} can be assigned to an agent {1} .", request.properties(), instance.name()));
-                return DefaultGoPluginApiResponse.success("true");
-        }
-
-
         LOG.info(format("[should-assign-work] Job with profile {0} can be assigned to an agent {1}", request.properties(), instance.name()));
         return DefaultGoPluginApiResponse.success("false");
-    }
-
-    private boolean matchUsingProfileAndEnvironment(DockerService instance) {
-        boolean environmentMatches = stripToEmpty(request.environment()).equalsIgnoreCase(stripToEmpty(instance.environment()));
-
-        Map<String, String> containerProperties = instance.properties() == null ? new HashMap<>() : instance.properties();
-        Map<String, String> requestProperties = request.properties() == null ? new HashMap<>() : request.properties();
-
-        return environmentMatches && requestProperties.equals(containerProperties);
     }
 }
